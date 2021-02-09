1bde0ae78a6cdbe560178e3a110f59c3514f5f03
LINEKING2100 <63192745+LINEKING2100@users.noreply.github.com>
2021-02-09 21:14:29 +0900

    Add files via upload

--- 검.js
+++ 검.js
@@ -1,46 +1,49 @@
-m = 1000
+m = 5000
 lv = 1
 up = 100
-d = 0
 b = 0
 qkdwl = 1
-
+upp = 100
+bb = 0
+llv = 1
 
 function up_click() {
     if (m >= 100 * lv) {
         m = m - (100 * lv) * qkdwl
         document.getElementById("돈").innerHTML = m + "원"
 
-        x = Math.floor(Math.random() * 100)
-        if (x+1 <= up) {
-            up = Math.ceil((up - up/10) * 100) / 100
-            d = Math.ceil(((100 - up) - (100 - up)/10)*100)/100
-            b = Math.ceil(((100 - up)/10)*100)/100
+        x = Math.floor(Math.random() * 10000)
+        x = x/100
+        console.log(x)
+        if (x <= up) {
+            up = Math.ceil((up - up/50) * 100) / 100
+            b = Math.ceil((100 - up)*100)/100
             lv = lv + 1
-            document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>실패확률:" + d + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
+            document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
 
             on_f()
         }
-        else if (100 - (x+1) <= d) {
-            console.log("풉")
-        }
         else {
             if (qkdwl == 1) {
+                console.log(x + "x")
                 alert("검 파괴됨")
+                
+                upp = up
+                bb = b
+                llv = lv
+
                 up = 100
-                d = 0
                 b = 0
                 lv = 1
-                document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>실패확률:" + d + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
+                document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
 
                 on_f()
             }
             else if (qkdwl == 2) {
-                up = Math.ceil((up - up/10) * 100) / 100
-                d = Math.ceil(((100 - up) - (100 - up)/10)*100)/100
-                b = Math.ceil(((100 - up)/10)*100)/100
+                up = Math.ceil((up - up/50) * 100) / 100
+                b = Math.ceil((100 - up)*100)/100
                 lv = lv + 1
-                document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>실패확률:" + d + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
+                document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
 
                 on_f()
             }
@@ -52,10 +55,9 @@ function asdf_click() {
     if (lv > 2) {
         m = m + 200 * lv * (lv/2)
         up = 100
-        d = 0
         b = 0
         lv = 1
-        document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>실패확률:" + d + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
+        document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
         document.getElementById("돈").innerHTML = m + "원"
 
         on_f()
@@ -65,12 +67,16 @@ function asdf_click() {
     }
 }
 
-function qkdwl_click() {
-    if (qkdwl == 1) {
-        qkdwl = 2
-    }
-    else {
-        qkdwl = 1
+function af_click() {
+    if (m >= (300 * llv)) {
+        m = m - (300 * llv)
+        up = upp
+        b = bb
+        lv = llv
+        document.getElementById("확률").innerHTML = "성공확률:" + up + "<br>파괴확률:" + b + "<br>" + 100 * lv + "원"
+        document.getElementById("돈").innerHTML = m + "원"
+
+        on_f()
     }
 }
 
@@ -98,5 +104,9 @@ function on_f() {
     if (lv == 6) {
         document.getElementById("lv").innerHTML = "Lv.6 투명검"
         document.getElementById("rja").src = "투명.png"
+    }
+    if (lv == 7) {
+        document.getElementById("lv").innerHTML = "Lv.7 코드검"
+        document.getElementById("rja").src = "코드검.png"
     }
 }
